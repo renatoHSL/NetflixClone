884049f51b4aef151afa1a0c17bcf62e722242c9
Renato <renato92.hora@gmail.com>
2023-12-12 11:02:37 -0300

    Auth finalizado

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -14,22 +14,24 @@ const Auth = () => {
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-            <div className="bg-black w-full h-full lg:bg-opacity-50">
+            <div className="bg-black w-full h-full lg, sm:bg-opacity-50">
                 <nav className="px-12 py5">
                     <img src="/images/logo.png" alt="logo" className="h-12"/>
                 </nav>
                 <div className="flex justify-center">
                     <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
                         <h2 className="text-white text-4xl mb-8 font-semibold">
-                            Sign in
+                            {variant === 'login' ? 'Sign in' : 'Register'}
                         </h2>
                         <div className="flex flex-col gap-4">
-                            <Input
-                                label="Username"
-                                onChange={(ev: any) => setName(ev.target.value)}
-                                id="name"
-                                value={name}
-                            />
+                            {variant === 'register' && (
+                                <Input
+                                    label="Username"
+                                    onChange={(ev: any) => setName(ev.target.value)}
+                                    id="name"
+                                    value={name}
+                                />
+                            )}
                             <Input
                                 label="Email"
                                 onChange={(ev: any) => setEmail(ev.target.value)}
@@ -46,12 +48,12 @@ const Auth = () => {
                             />
                         </div>
                         <button className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-                            Login
+                            {variant === 'login' ? 'Login' : 'Sign up'}
                         </button>
                         <p className="text-neutral-500 mt-12">
-                            First time using Netflix?
-                            <span className="text-white ml-1 hover:underline cursor-pointer">
-                                Create an account
+                            {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                                {variant === 'login' ? 'Create an account' : 'Login'}
                             </span>
                         </p>
                     </div>
