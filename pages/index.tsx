5b1a12898513edded4845820b6253b249dfbf764
Renato <renato92.hora@gmail.com>
2023-12-20 11:12:36 -0300

    Finalizando navbar e interações da pagina de profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { NextPageContext } from "next"
-import { getSession, signOut } from "next-auth/react"
+import { getSession } from "next-auth/react"
 
-import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
+
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -21,14 +22,11 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-const { data: user } = useCurrentUser();
 // TODO VERIFICAR PORQUE RETORNA USER COMO TEST
-
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix Clone</h1>
-      <p className="text-white">Logged in as : {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout!</button>
+      <Navbar />
     </>
   )
 }
+
