076e0c777ecdd4ab65d162f92c5e4c21ac67683c
Renato <renato92.hora@gmail.com>
2023-12-20 18:22:24 -0300

    Finalizado billboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPageContext } from "next"
 import { getSession } from "next-auth/react"
 
 import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
 
 
 export async function getServerSideProps(context: NextPageContext) {
@@ -26,6 +27,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   )
 }
